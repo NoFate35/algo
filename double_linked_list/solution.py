007798ed3ad1670fec8334a327ab0a76bf69768a
NoFate35 <vsokol35@mail.ru>
2026-06-21 13:28:53 +0300

    Changes to be committed: 	modified:   double_linked_list/solution.py

--- double_linked_list/solution.py
+++ double_linked_list/solution.py
@@ -4,9 +4,9 @@ from helpers import get_double_linked_list
 def solution(items):
     if items == []:
         return items
-    del_value = items[1]
+    sec_value = items[1]
     double_linked_list = get_double_linked_list(items)
-    deleted_to_first_node = double_linked_list.delete(del_value)
+    deleted_to_first_node = double_linked_list.delete(sec_value)
     double_linked_list.prepend(deleted_to_first_node.value)
     return double_linked_list.to_array()
 
